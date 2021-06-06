b2fd2aae1b4d896391dc801f70eff5b305d30b5a
vijaynaidu013 <84774714+vijaynaidu013@users.noreply.github.com>
2021-06-06 14:22:31 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hi Vijay, Greetings from Springboot..!!!";
+        return "Hi Priya Nawal, Greetings from Springboot..!!!";
     }
     
 }
